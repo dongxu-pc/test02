2e6c4637d80912db8a462de2e9ea3a96cef0b966
dong <gwmxyd@163.com>
2015-12-06 22:13:28 +0800

    static const

--- src/format.c
+++ src/format.c
@@ -29,6 +29,7 @@ inline uint64_t  varToint64(varint* vint)
 	return value;
 }
 
+/* 将key解析为key1和key2（sequence）两个部分那 */
 inline size_t parseKey(const Slice* key,Slice* key1,Slice* key2)
 {
 	int i = 0;
@@ -49,9 +50,35 @@ inline size_t parseKey(const Slice* key,Slice* key1,Slice* key2)
 	return 0;
 }
 
+/* 在key中添加 */
+inline size_t appendKey()
+/* key1 > key2 则为1，反之为-1，相等则为0 */
 inline size_t comparaKey(Slice key1,Slice key2){
-	if(key1.size_>key2.size_){
-		if(strcmp(key1.data_,key2.data_))
+	size_t min_size = (key1.size_ > key2.size_t) ? key2.size_ : key1.size_;
+	size_t i;
+	/* 比较key */
+	for(i = 0;i < (min_size-8);i++){
+		if(key1.data_[i] > key2.data_[i]){
+			return 1;
+		}else if(key1.data_[i] < key2.data_[i]){
+			return -1;
+		}
 	}
 	
+	if(key1.size_ < key2.size_){
+		return 1;
+	}else if(key1.size_ > key2.size_){
+		return -1;
+	}
+	
+	return 0;
+}
+
+/* sequenceNum是随着数据项增加，越大说明越新 */
+inline size_t comparaKeySequence(Slice key1,Slice key2){
+	if(decodeFixed64(key1.data_+key1.size_-8) > decodeFixed64(key2.data_+key2.size_-8)){
+		return 1;
+	}else{
+		return -1;
+	}
 }
